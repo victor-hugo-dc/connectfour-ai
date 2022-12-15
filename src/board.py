2dac5ae18daa2cb9c6740dfd64b5051ba5e4a9f7
Victor Del Carpio <victor.delcarpiogomez@yale.edu>
2022-12-15 02:32:35 -0500

    bitboard implementation of connect four

--- src/board.py
+++ src/board.py
@@ -1,98 +1,58 @@
-class ConnectFour:
+class Board:
 
-    EMPTY_CELL = 0
-    PLAYER_ONE = 1
-    PLAYER_TWO = 2
+    WIDTH: int = 7
+    HEIGHT: int = 6
+    TOKENS: list = [
+        [7, 4], # Horizontal Connect-Four
+        [1, 2], # Vertical Connect-Four
+        [6, 12], # Diagonal Connect-Four
+        [8, 16] # Diagonal Connect-Four
+    ]
 
-    def __init__(self, rows: int, cols: int) -> None:
-        self.rows = rows
-        self.cols = cols
-        self.board = [[ConnectFour.EMPTY_CELL for _ in range(cols)] for _ in range(rows)]
-
-    def clone(self):
-        result = ConnectFour(self.rows, self.cols)
-        result.board = [row[:] for row in self.board]
-        return result
-
-    # places the move in the correct column and updates the board,
-    # if it cant be played then it returns an error, otherwise it
-    # returns the boolean value of whether or not sequence of four
-    # same colored disks for the input player is in the board
-    def move(self, col: int, player: int) -> bool:
-        
-        if not (0 <= col < self.cols):
-            raise ValueError(f'Poor column value: {col}')
-        
-        if player not in [ConnectFour.PLAYER_ONE, ConnectFour.PLAYER_TWO]:
-            raise ValueError(f'Invalid player argument: {player}')
-
-        for row in range(self.rows):
-            if self.board[row][col] == ConnectFour.EMPTY_CELL:
-                self.board[row][col] = player
-                return self.check_connect_four((row, col), player)
-                
-        
-        raise ValueError(f'Column {col} is full')
+    def __init__(self) -> None:
+        '''
+        position: bitstring representing the positions of the tokens of a player
+        mask: bitstring representing the positions of both players
+        '''
+        self.position: int = 0
+        self.mask: int = 0
+        self.moves = 0
     
-    # this checks if we have created a connect four by expanding from the current
-    # position in each opposing direction, so we can check verticals, horizontals,
-    # and both diagonals.
-    # TODO: refactor for less code tbh
-    def check_connect_four(self, position: tuple, player: int) -> bool:
+    def clone(self):
+        result: Board = Board()
+        result.position = self.position
+        result.mask = self.mask
+        result.moves = self.moves
+        return result
+    
+    def is_playable(self, col: int) -> bool:
+        top_mask: int = 1 << (Board.HEIGHT - 1) << col * (Board.HEIGHT + 1)
+        return (self.mask & top_mask) == 0
 
-        north = self._expand(position, (1, 0), player)
-        south = self._expand(position, (-1, 0), player)
+    def connected_four(self) -> bool:
+        line: int
 
-        vertical = north + south - 1
-        if vertical >= 4:
-            return True
-
-        west = self._expand(position, (0, -1), player)
-        east = self._expand(position, (0, 1), player)
-
-        horizontal = west + east - 1
-        if horizontal >= 4:
-            return True
-
-        northeast = self._expand(position, (1, 1), player)
-        southwest = self._expand(position, (-1, -1), player)
-
-        diag_nesw = northeast + southwest - 1
-        if diag_nesw >= 4:
-            return True
-
-        northwest = self._expand(position, (1, -1), player)
-        southeast = self._expand(position, (-1, 1), player)
-
-        diag_nwse = northwest + southeast - 1
-        if diag_nwse >= 4:
-            return True
+        # Use the position bitboard to determine a win
+        for [a, b] in Board.TOKENS:
+            line = self.position & (self.position >> a)
+            if (line & (line >> b)):
+                return True
 
         return False
-
-
-    # position is passed in as a tuple of (row, col)
-    # player is either PLAYER_ONE or PLAYER_TWO
-    def _expand(self, position: tuple, delta: tuple, player: int) -> int:
-        row, col = position
-        row_delta, col_delta = delta
-        count: int = 0
-
-        # i think we cna remove the try catch finally now that were checking
-        # for the bounds (this was taken from the other code)
-        try:
-            while (0 <= row < self.rows) and (0 <= col < self.cols) \
-                and self.board[row][col] == player:
-
-                count += 1
-                row += row_delta
-                col += col_delta
+    
+    def move(self, col: int) -> bool:
+        if not (0 <= col < Board.WIDTH):
+            raise ValueError
         
-        finally:
-            return count
+        if not self.is_playable(col):
+            raise ValueError
 
+        bottom_mask: int = 1 << (col * (Board.HEIGHT + 1))
+        self.position ^= self.mask
+        self.mask |= self.mask + bottom_mask
+        self.moves += 1
 
-    # if you think about it we only have to check the last value of each row
-    # to know what columns are available to be played on
-    def get_valid_moves(self):
-        return [i for i, v in enumerate(self.board[self.rows - 1]) if v == ConnectFour.EMPTY_CELL]
+        return self.connected_four()
+    
+    def get_valid_moves(self) -> list:
+        return [c for c in range(Board.WIDTH) if self.is_playable(c)]
