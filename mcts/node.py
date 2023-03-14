d2d8f0d0355e79f38f8288959d7acc6ab7cfec57
victordelcarpio <victor.delcarpiogomez@yale.edu>
2023-03-14 17:43:08 -0400

    more accurate type annotations

--- mcts/node.py
+++ mcts/node.py
@@ -1,14 +1,14 @@
 from game.connectfour import ConnectFour
 
 class Node:
-    def __init__(self, game: ConnectFour, game_copy: ConnectFour = None, parent: 'Node' = None):
+    def __init__(self, game: ConnectFour, action: int = None, parent: 'Node' = None):
         self.game: ConnectFour = game.clone()
-        self.game_copy = game_copy
+        self.action: int = action
         self.parent: 'Node' = parent
-        self.actions = game.get_valid_moves()
-        self.children = []
+        self.actions: list[int] = game.get_valid_moves()
+        self.children: list = []
         self.payoff: int = 0
-        self.num_paths: int = 0
+        self.visits: int = 0
     
     @property
     def q(self) -> float:
